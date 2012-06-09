2467ba48843ebde7e52690b2bf62443440319082
dahlia <dlbock@gmail.com>
2012-06-09 18:03:53 -0400

    dahlia & mykola: adding service call to create a commitment

--- lib/api/crowdshift_service.rb
+++ lib/api/crowdshift_service.rb
@@ -8,5 +8,13 @@ module Api
       self.class.post('/event', options)
     end
 
+    def create_commitment(event_api_id, start_date, end_date)
+      start_date = start_date.utc.strftime("%Y-%m-%dT%H:%M:%S")
+      end_date = end_date.utc.strftime("%Y-%m-%dT%H:%M:%S")
+      options = { :headers => { 'authorization' => "bearer 30a17a7e-faeb-46dd-a54c-94f36b9e1464"},
+                  :body => { 'start' => start_date, 'end' => end_date } }
+      self.class.post("/event/#{event_api_id}/commitment", options)
+    end
+
   end
 end
